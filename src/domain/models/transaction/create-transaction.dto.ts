db8d6352417b6d38c8344766bc934ff1da51f71d
Makstorm <mxm.horobets@gmail.com>
2024-02-05 22:12:33 +0200

    fix: fixed pattern where user can create  transactions with negative and zero amount

--- src/domain/models/transaction/create-transaction.dto.ts
+++ src/domain/models/transaction/create-transaction.dto.ts
@@ -1,18 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsPositive, Min } from 'class-validator';
 // import { TransactionType } from 'src/core';
 
 export class CreateTransactionDto {
   @ApiProperty({ type: Number })
+  @IsPositive()
+  @Min(1)
   public amount: number;
-
-  // @ApiProperty({
-  //   description: 'List of posible variants',
-  //   enum: TransactionType,
-  //   isArray: false,
-  //   example: TransactionType.CONSUMABLE,
-  // })
-  // public type: TransactionType = TransactionType.CONSUMABLE;
 
   @ApiProperty({ type: String, example: 'cool@email.com' })
   @IsEmail()
